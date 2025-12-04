86bcb223bfb361f4268a1254b7b5095968584229
yune22 <wjddbs020604@gmail.com>
2025-12-04 20:16:54 +0900

    search

--- sql/DML/admin/team_management.sql
+++ sql/DML/admin/team_management.sql
@@ -1,4 +1,4 @@
--- 구단 등록
+-- ADMT-01 구단 등록 --
 insert into team_list(ground_id, admin_id, team_name) values(15, '1b15a477-d0e7-11f0-94f4-02001702205f', '삼성');
 insert into team_list(ground_id, admin_id, team_name) values(16, '21c66e2e-d0e7-11f0-94f4-02001702205f', '롯데');
 insert into team_list(ground_id, admin_id, team_name) values(14, '21c66e2e-d0e7-11f0-94f4-02001702205f', '한화');
@@ -10,7 +10,7 @@ insert into team_list(ground_id, admin_id, team_name) values(18, '1b15a477-d0e7-
 insert into team_list(ground_id, admin_id, team_name) values(10, '233d13d4-d0e7-11f0-94f4-02001702205f', '두산');
 insert into team_list(ground_id, admin_id, team_name) values(11, '20ada1ff-d0e7-11f0-94f4-02001702205f', '키움');
 
--- 구단 수정 (구단의 구장 수정)
+-- ADMT-02 구단 수정 (구단의 구장 수정) --
 update team_list set team_name='삼성' where groun_id=12;
 update team_list set team_name='롯데' where groun_id=15;
 update team_list set team_name='한화' where groun_id=17;
@@ -22,7 +22,7 @@ update team_list set team_name='SSG' where groun_id=17;
 update team_list set team_name='NC' where groun_id=13;
 update team_list set team_name='KT' where groun_id=17;
 
--- 구단 삭제
+-- ADMT-03 구단 삭제 --
 alter table team_list add column team_status tinyint not null default 1;
 update team_list set team_status=0 where team_id=10;
 select * from team_list where team_status=1;
--- sql/DML/search/team_seach.sql
+++ sql/DML/search/team_seach.sql
@@ -1,2 +1,3 @@
--- 현재 존재하는 구단 정보 조회
-select * from team_list where team_status=1;
+-- TEAM-01 현재 존재하는 구단 정보 조회 (구단 이름, 구단의 구장, 구장 주소, 구장 좌석수, 좌석 이름, 좌석 시야, 좌석 등급, 가격)
+select t.team_name, g.area, g.ground_name, g.address, g.seat_count, s.seat_name, s.ground_sight, s.ground_grade, s.price from team_list t inner join ground_list g on t.ground_id=g.ground_id inner join seat_list s on t.ground_id=s.ground_id where t.team_status=1;
+
