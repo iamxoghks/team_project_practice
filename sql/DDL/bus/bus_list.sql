122223767a50247505fb95aea7c2d18283035915
iamyourkeyring <181794569+iamxoghks@users.noreply.github.com>
2025-12-04 20:59:57 +0900

    bus sql change

--- sql/DDL/bus/bus_list.sql
+++ sql/DDL/bus/bus_list.sql
@@ -1,7 +1,7 @@
 CREATE TABLE bus_list(
     bus_id BIGINT NOT NULL auto_increment,
-    grund_id BIGINT NOT NULL,
-    admin_id CHAR(36) NOT NULL,
+    ground_id BIGINT NOT NULL,
+    admin_id VARCHAR(36) NOT NULL,
     bus_seat_count INT,
     PRIMARY KEY (bus_id),
     FOREIGN KEY (admin_id) REFERENCES USER_LIST (user_id)
