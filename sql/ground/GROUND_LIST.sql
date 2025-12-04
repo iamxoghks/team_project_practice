27564c11264298269ec4900bf99f54ba74d9803b
kimdogyun <sydg1214@gmail.com>
2025-12-04 16:30:38 +0900

    ground

--- sql/ground/GROUND_LIST.sql
+++ sql/ground/GROUND_LIST.sql
@@ -1,5 +1,7 @@
-create table GROUND_LIST
-(ground_id bigint not null auto_increment,admin_id varchar(36) not null,
-area varchar(255) not null,ground_name varchar(255),
-address varchar(255) not null,seat_count bigint not null,primary key(ground_id),
-foreign key (admin_id) references admin(admin_id));
+create table GROUND_LIST(
+    ground_id bigint not null auto_increment,admin_id varchar(36) not null,
+    area varchar(255) not null,ground_name varchar(255),
+    address varchar(255) not null,seat_count bigint not null,
+    primary key(ground_id),
+    foreign key (admin_id) references admin(admin_id)
+);
