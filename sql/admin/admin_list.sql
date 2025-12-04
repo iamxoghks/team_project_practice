f0c19cf96b71a06c09ac0476171e3dd932e2c7c0
lampshub <dkdkwlsgml@gmail.com>
2025-12-04 16:19:54 +0900

    fix

--- sql/admin/admin_list.sql
+++ sql/admin/admin_list.sql
@@ -1,4 +1,7 @@
 create database mhit;
 use mhit;
 
-create table admin_list(admin_id varchar(36) not null primary key default (uuid()),admin_name varchar(255) not null, admin_password varchar(255) not null);
+create table admin_list(
+admin_id varchar(36) not null primary key default (uuid()),
+admin_name varchar(255) not null, 
+admin_password varchar(255) not null);
