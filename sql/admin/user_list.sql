4da29c06b757e81f01be88fb3331fcab3de057a3
lampshub <dkdkwlsgml@gmail.com>
2025-12-04 17:34:20 +0900

    fix

--- sql/admin/user_list.sql
+++ sql/admin/user_list.sql
@@ -4,5 +4,8 @@ user_name varchar(255) not null,
 admin_password varchar(255) not null, 
 user_phone varchar(255) not null, 
 user_email varchar(255) not null, 
-user_fav_team bigint, 
-foreign key (user_fav_team) references team_list(team_id) on delete set null on update cascade);
+user_fav_team bigint);
+
+alter table user_list add constraint 
+foreign key (user_fav_team) references team_list(team_id) 
+on delete set null on update cascade;
