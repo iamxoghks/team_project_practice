ed447bb165cff07d060638886134f18b0a3d7e88
yune22 <wjddbs020604@gmail.com>
2025-12-05 10:20:23 +0900

    d

--- sql/DML/reservation/game_res.sql
+++ sql/DML/reservation/game_res.sql
@@ -1,13 +1,21 @@
 -- GRESERVE-01 경기 예매 --
 insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
-('17d725dd-d10a-11f0-94f4-02001702205f', 74, 2, '2025-12-04 21:00:00'),
-('17d726fe-d10a-11f0-94f4-02001702205f', 75, 1, '2025-12-04 21:00:00'),
-('17d72745-d10a-11f0-94f4-02001702205f', 76, 2, '2025-12-05 21:00:00'),
-('17d725dd-d10a-11f0-94f4-02001702205f', 77, 3, '2025-12-06 21:00:00'),
-('17d726fe-d10a-11f0-94f4-02001702205f', 78, 1, '2025-12-07 21:00:00'),
-('17d72745-d10a-11f0-94f4-02001702205f', 79, 2, '2025-12-08 21:00:00'),
-('17d727bf-d10a-11f0-94f4-02001702205f', 80, 2, '2025-12-09 21:00:00'),
-('17d725dd-d10a-11f0-94f4-02001702205f', 81, 1, '2025-12-07 21:00:00'),
+('17d725dd-d10a-11f0-94f4-02001702205f', 74, 2, '2025-12-04 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d726fe-d10a-11f0-94f4-02001702205f', 75, 1, '2025-12-04 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d72745-d10a-11f0-94f4-02001702205f', 76, 2, '2025-12-05 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d725dd-d10a-11f0-94f4-02001702205f', 77, 3, '2025-12-06 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d726fe-d10a-11f0-94f4-02001702205f', 78, 1, '2025-12-07 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d72745-d10a-11f0-94f4-02001702205f', 79, 2, '2025-12-08 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d727bf-d10a-11f0-94f4-02001702205f', 80, 2, '2025-12-09 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
+('17d725dd-d10a-11f0-94f4-02001702205f', 81, 1, '2025-12-07 21:00:00');
+insert into game_res_list(user_id, game_id, game_res_count, game_res_date) values
 ('17d727bf-d10a-11f0-94f4-02001702205f', 82, 2, '2025-12-05 21:00:00');
 
 
